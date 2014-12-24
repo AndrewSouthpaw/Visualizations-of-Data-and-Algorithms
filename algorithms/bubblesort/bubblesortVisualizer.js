0883f182447c56cf83bc04b403cc53e18f5f1365
Andrew Smith <andrewsmith@alumni.stanford.edu>
2014-12-23 18:10:58 -0800

    add button to compare the two implementations

--- algorithms/bubblesort/bubblesortVisualizer.js
+++ algorithms/bubblesort/bubblesortVisualizer.js
@@ -8,7 +8,7 @@ var BAR_WIDTH = 50;
 var BAR_Y_AXIS = 200;
 var MAX_NUMBER = 100;
 var MIN_NUMBER = 10;
-var ANIMATION_DURATION = 250;
+var ANIMATION_DURATION = 100;
 var dataArray1; // A global variable that holds the unsorted array.
                // Initialized as a random array. Manipulated by bubbleSort.
 var dataArray2; // A global variable that holds the unsorted array.
@@ -115,106 +115,25 @@ var createRandomArray = function(length, min, max){
   return result;
 }
 
+
 /**
  * bubbleSort
  * ==========
- * Runs the bubble sort algorithm. Requires JavaScript backflips to pause
- * processing of array to allow animation to proceed.
+ * Handles sorting and visualization of sorting process. Can specify 'faster'
+ * to specify a faster implementation of bubble sort.
  */
 
-//  var bubbleSort = function(data) {
-//   andThen.reset();
-//   if (data.length < 2) return data;
-//   // Begin on second element in array, and compare with previous values.
-//   // Swap if necessary.
-//   // Keep track of whether the array is sorted.
-//   var sorted = true;
-//   for(var i = 1; i < data.length; i++) {
-//     andThen.doThis((function() {
-//       var j = i;
-//       return function() {
-//         highlightBars([data[j - 1], data[j]]);
-//       }
-//     })(),250);
-//     var delay = 10;
-//     andThen.doThis((function() {
-//       var j = i;
-//       return function() {
-//         if (data[j - 1].num > data[j].num) {
-//           // If swap occurs: mark array as unsorted.
-//           var temp = data[j];
-//           data[j] = data[j - 1];
-//           data[j - 1] = temp;
-//           sorted = false;
-//           delay = ANIMATION_DURATION; // set delay to wait for swap animation
-//           update();
-//         }
-//       };
-//     })(), 250);
-//     andThen.doThis(function() {
-//       clearHighlight();
-//     }, delay);
-//   }
-//   andThen.doThis(function() {
-//     console.log(sorted)
-//     return sorted ? data : bubbleSort(data);
-//   }, ANIMATION_DURATION);
-// };
-
-
-
-var bubbleSort = function(data) {
-  var svg = d3.select('#sort-regular');
-
-  if (data.length < 2) return data;
-  var sorted = true;
-
-  // Recursive approach to call iterations within setTimeout delay
-  function iterate (i) {
-    if (i >= data.length) {
-      return sorted ? data : bubbleSort(data);
-    }
-
-    // Show bars being compared
-    highlightBars(svg, [data[i - 1], data[i]]);
-
-    // Call remainder after animation for highlightBars
-    setTimeout(function() {
-      // Set default delay
-      var delay = 10;
-
-      // Compare elements, swap, and animate
-      if (data[i - 1].num > data[i].num) {
-        var temp = data[i];
-        data[i] = data[i - 1];
-        data[i - 1] = temp;
-        sorted = false;
-        delay = ANIMATION_DURATION; // set delay to wait for swap animation
-        update(svg, dataArray1);
-      }
-
-      // Call remainder after animation for swap
-      setTimeout(function() {
-        clearHighlight(svg);
-
-        // Call remainder after animation for removing highlights
-        setTimeout(function() {
-          iterate(i + 1);
-        }, 250);
-      }, delay);
-    }, 250);
-  }
-
-  // Initiate recursion
-  iterate(1);
-};
-
-
-var bubbleSortFaster = function(data, start) {
+var bubbleSort = function(algorithm, data, start) {
   // Store svg for later reference
-  var svg = d3.select('#sort-faster');
   
-  var start = start || 1;
+  var svg, start;
+  if (algorithm === 'faster') {
+    svg = d3.select('#sort-faster');
+    start = start || 1;
+  } else {
+    svg = d3.select('#sort-regular');
+    start = 1;
+  }
 
   // Reset andThen timer for each recursive round
   andThen.reset();
@@ -247,7 +166,7 @@ var bubbleSortFaster = function(data, start) {
             sorted = false;
             swapPoint = j-1;
           } 
-          update(svg, dataArray2);
+          update(svg, data);
         }
       };
     })(), ANIMATION_DURATION);
@@ -266,7 +185,7 @@ var bubbleSortFaster = function(data, start) {
       // data up to that point, then concat the rest of the array which will be
       // sorted. If swapPoint is already 0, keep it at the beginning of the
       // array.
-      return bubbleSortFaster(data, swapPoint);
+      return bubbleSort(algorithm, data, swapPoint);
     }
     
   },ANIMATION_DURATION);
